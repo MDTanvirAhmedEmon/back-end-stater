edcde6c44383fdaa16ac19f96ef4c9d634f30643
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-09-27 16:27:30 +0600

    set up dotenv and cors

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
 import app from "./app"
 import mongoose from 'mongoose';
-const port = 5000
+import config from "./config";
 
 async function main() {
-
     try {
-        await mongoose.connect(process.env.MONGODB_URL as string);
-        app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`)
+        await mongoose.connect(config.database_url as string);
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
         })
     }
     catch (error) {
         console.log(error)
     }
-
 }
 
 main()

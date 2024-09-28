466407831f788ec9959c5473e632bb5bafcd65e3
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-09-28 16:40:35 +0600

    created not found api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import router from './app/routes'
+import notFound from './app/middlewares/notFound'
 const app: Application = express()
 
 // parser
@@ -9,8 +10,10 @@ app.use(cors())
 
 app.use('/api/v1', router)
 
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+app.use(notFound)
 
 export default app
